00c7d3ee6dc3ea4ff277f1ae6382cf584605a4ee
Dream-Team-Project <abhishek@dreamreflectionmedia.com>
2022-05-24 13:42:14 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgApexchartsModule } from "ng-apexcharts";
+import { GoogleMapsModule } from '@angular/google-maps';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -12,16 +15,13 @@ import { MatSliderModule } from '@angular/material/slider'
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MAT_COLOR_FORMATS, NgxMatColorPickerModule, NGX_MAT_COLOR_FORMATS } from '@angular-material-components/color-picker';
-import { NgApexchartsModule } from "ng-apexcharts";
-import { GoogleMapsModule } from '@angular/google-maps';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatCardModule} from '@angular/material/card';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatSelectModule} from '@angular/material/select';
+
 
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,6 +38,7 @@ import { NgxColorsModule } from 'ngx-colors';
 import { NgxTinymceModule } from 'ngx-tinymce';
 import { SafeHtmlPipe } from './safe-html.pipe';
 import { EditorModule } from '@tinymce/tinymce-angular';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 
 @NgModule({
   declarations: [
@@ -45,11 +46,8 @@ import { EditorModule } from '@tinymce/tinymce-angular';
     RoutingComponents,
     SidebarComponent,
     NavbarComponent,
-<<<<<<< HEAD
     ForgetPasswordComponent,
     SafeHtmlPipe,
-=======
->>>>>>> 9550893dbe0702d5024119648ad3039151129034
   ],
   imports: [
     BrowserModule,
@@ -60,41 +58,32 @@ import { EditorModule } from '@tinymce/tinymce-angular';
     HttpClientModule,
     SortablejsModule,
     DragDropModule,
-<<<<<<< HEAD
     NgxMatColorPickerModule,
     NgxColorsModule,
     MatFormFieldModule,
     MatProgressBarModule,
-    MatTooltipModule,
     MatTabsModule,
     MatButtonModule,
     MatSnackBarModule,
     MatSliderModule,
-    MatSelectModule,
     MatIconModule,
     MatRadioModule,
     MatButtonToggleModule,
     ColorMaterialModule,
     ColorCircleModule,
     EditorModule,
-    NgxTinymceModule.forRoot({
-      baseURL: '//cdnjs.cloudflare.com/ajax/libs/tinymce/5.7.1/'
-    })
-=======
     MatTooltipModule,
-    BrowserModule,
-    FormsModule,
     NgApexchartsModule,
     GoogleMapsModule,
     MatInputModule,
-    ReactiveFormsModule,
-    MatButtonModule,
     MatPaginatorModule,
     MatCardModule,
     MatCheckboxModule,
     MatSelectModule,
+    NgxTinymceModule.forRoot({
+      baseURL: '//cdnjs.cloudflare.com/ajax/libs/tinymce/5.7.1/'
+    })
   
->>>>>>> 9550893dbe0702d5024119648ad3039151129034
   ],
   providers: [authInterceptorProviders, { provide: MAT_COLOR_FORMATS, useValue: NGX_MAT_COLOR_FORMATS }],
   bootstrap: [AppComponent]
