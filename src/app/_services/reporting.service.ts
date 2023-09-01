7600d77ea1cd57f1503c69dd071ee5f3839f0c04
Dream-Team-Project <abhishek@dreamreflectionmedia.com>
2023-09-01 09:33:13 +0530

    report

--- src/app/_services/reporting.service.ts
+++ src/app/_services/reporting.service.ts
@@ -9,8 +9,12 @@ import { TokenStorageService } from './token-storage.service';
 export class ReportingService {
 
   user_id:any;
-  recentContactsApi = '/api/recentcontacts/';
-  monthlyContactsApi = '/api/monthlycontacts/';
+
+  recentContactsApi = '/api/recentcontacts';
+  datefilterContactsApi = '/api/datefiltercontacts';
+
+  recentCampaignsApi = '/api/recentcampaigns';
+  datefilterCampaignsApi = '/api/datefiltercampaigns';
 
   constructor(private http: HttpClient, private tokenStorage: TokenStorageService) {
     this.user_id = this.tokenStorage.getUser().uniqueid;
@@ -21,8 +25,18 @@ export class ReportingService {
     .pipe(catchError(this.errorHandler));
   }
 
-  monthlyContacts(): Observable<any> {
-    return this.http.get(this.monthlyContactsApi+'/'+this.user_id)
+  datefilterContacts(from:Date, to:Date): Observable<any> {
+    return this.http.get(this.datefilterContactsApi+'/'+this.user_id+'/'+from+'/'+to)
+    .pipe(catchError(this.errorHandler));
+  }
+
+  recentCampaigns(limit:number): Observable<any> {
+    return this.http.get(this.recentCampaignsApi+'/'+this.user_id+'/'+limit)
+    .pipe(catchError(this.errorHandler));
+  }
+
+  datefilterCampaigns(from:Date, to:Date): Observable<any> {
+    return this.http.get(this.datefilterCampaignsApi+'/'+this.user_id+'/'+from+'/'+to)
     .pipe(catchError(this.errorHandler));
   }
   
