529f5282107262277fd12297668781eaa7f43bca
Dreamtulsi <teamlead@dreamreflectionmedia.com>
2023-09-18 13:53:23 +0530

    page_preview

--- src/app/_components/page-view/page-view.component.ts
+++ src/app/_components/page-view/page-view.component.ts
@@ -7,6 +7,8 @@ import { ElementService } from 'src/app/_services/_builder/element.service';
 import { StyleService } from 'src/app/_services/_builder/style.service';
 import { ImageService } from 'src/app/_services/image.service';
 import { FileUploadService } from 'src/app/_services/file-upload.service';
+import { Meta, Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-page-view',
@@ -18,6 +20,8 @@ export class PageViewComponent implements OnInit {
 
   @Input ('target') target:any = 'main';
   @Input ('target_id') target_id:any;
+  @Input ('target_sections') target_sections:any;
+  @Input ('target_style') target_style:any;
 
   req = {
     uid: '',
@@ -25,16 +29,18 @@ export class PageViewComponent implements OnInit {
     pid: '',
   }
   page_json:any = {
+    header: {sections: []},
+    footer: {sections: []},
     sections: [],
     mainstyle: '',
-    header: {id: ''},
-    footer: {id: ''}
   };
 
   constructor(
     private route: ActivatedRoute, 
     private webpage: WebpagesService, 
     private funnel: FunnelService,
+    private meta: Meta,
+    private title: Title,
     private _general: GeneralService,
     private _element: ElementService,
     private _file: FileUploadService,
@@ -44,53 +50,112 @@ export class PageViewComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
+      let view_type = params.get('view_type');
       let param_target = params.get('view_target');
-      let id = params.get('view_id')?.split('-');
-      if(id && this.target == 'main') {
-        this.req.uid = id[0];
-        this.req.wid = id[1];
-        this.req.pid = id[2];
+      let uid = params.get('user_id');
+      let wid = params.get('website_id');
+      let pid = params.get('page_id');
+      let tempid = params.get('template_id');
+      if(view_type == 'preview' && param_target && uid && wid && pid && this.target == 'main') {
+        this.req.uid = uid
+        this.req.wid = wid;
+        this.req.pid = pid;
         if(param_target == 'website') {
           this.webpage.getpreviewWebpage(this.req).subscribe((resp:any)=>{
-            this.page_json = this._general.decodeJSON(resp.data);
-            console.log(this.page_json)
-            this._general.fetchMenus().then(resp => {
-              this.setMenu(this.page_json.sections, resp);
-            })
+            console.log(resp)
+            if(resp?.data && resp?.data.length > 1) {
+              this.page_json = this._general.decodeJSON(resp.data);
+              this.loadScript(this.page_json.tracking.header, document.head);
+              this.addHead(this.page_json.head);
+              this.loadScript(this.page_json.tracking.footer, document.body);
+              document.head.innerHTML += this.page_json.head;
+              this._general.fetchMenus().then(resp => {
+                this.setMenu(this.page_json.sections, resp);
+              })
+            }
+            else this._general.redirectToPageNotFound();
           })
         }
         else if(param_target == 'funnel') {
-          this.funnel.getpreviewfunnelstep(this.req).subscribe((resp:any)=>{
+          this.funnel.getpreviewfunnelstep(this.req.pid).subscribe((resp:any)=>{
+            if(resp?.data && resp?.data.length > 1) {
+              this.page_json = this._general.decodeJSON(resp.data);
+              this.loadScript(this.page_json.tracking.header, document.head);
+              this.addHead(this.page_json.head);
+              this.loadScript(this.page_json.tracking.footer, document.body);
+              document.head.innerHTML += this.page_json.head;
+              this._general.fetchMenus().then(resp => {
+                this.setMenu(this.page_json.sections, resp);
+              })
+            }
+            else this._general.redirectToPageNotFound();
+          })
+        }
+        else this._general.redirectToPageNotFound();
+      }
+      else if(view_type == 'template' && tempid) {
+        let obj = {
+          tempid: tempid,
+          uid: this.req.uid,
+        }
+        this._file.previewpagetemplate(obj).subscribe((resp:any)=>{
+          if(resp?.data && resp?.data.length > 1) {
             this.page_json = this._general.decodeJSON(resp.data);
-            console.log(this.page_json)
+            this.addHead(this.page_json.head);
+            document.head.innerHTML += this.page_json.head;
             this._general.fetchMenus().then(resp => {
               this.setMenu(this.page_json.sections, resp);
             })
-          })
-        }
+          }
+          else this._general.redirectToPageNotFound();
+        })
       }
     })
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if(changes['target_id']) this.fetchTarget();
+    if(changes['target_sections']) {
+      const style = document.createElement('style');
+      style.innerHTML = this.target_style;
+      style.id = 'kb-'+this.target+'-style';
+      document.head.appendChild(style);
+      this.page_json.sections = this.target_sections;
+      this._general.fetchMenus().then(resp => {
+        this.setMenu(this.page_json.sections, resp);
+      })
+    }
+    else if(changes['target_id']) this.fetchTarget();
   }
 
   fetchTarget() {
     if(this.target == 'header') {
       this._file.getheader(this.target_id).subscribe((resp:any)=>{
-        this.page_json = this._general.decodeJSON(resp.data[0].json);
-        this._general.fetchMenus().then(resp => {
-          this.setMenu(this.page_json.sections, resp);
-        })
+        if(resp.data[0].json) {
+          this.page_json = this._general.decodeJSON(resp.data[0].json);
+          const style = document.createElement('style');
+          style.innerHTML = this.page_json.style;
+          style.id = 'kb-header-style';
+          document.head.appendChild(style);
+          this._general.fetchMenus().then(resp => {
+            this.setMenu(this.page_json.sections, resp);
+          })
+        }
+        else this.page_json.sections = [];
       })
     }
     else if(this.target == 'footer') {
       this._file.getfooter(this.target_id).subscribe((resp:any)=>{
-        this.page_json = this._general.decodeJSON(resp.data[0].json);
-        this._general.fetchMenus().then(resp => {
-          this.setMenu(this.page_json.sections, resp);
-        })
+        if(resp.data[0].json) {
+          this.page_json = this._general.decodeJSON(resp.data[0].json);
+          const style = document.createElement('style');
+          style.innerHTML = this.page_json.style;
+          style.id = 'kb-footer-style';
+          document.head.appendChild(style);
+          this._general.fetchMenus().then(resp => {
+            this.setMenu(this.page_json.sections, resp);
+          })
+        }
+        else this.page_json.sections = [];
       })
     }
   }
@@ -115,4 +180,40 @@ export class PageViewComponent implements OnInit {
     })
   }
 
+  addHead(head:any) {
+    const link:any = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'icon';
+    link.href = '/assets/uploads/images/keaimage-favicon-'+this.req.wid+'.png';
+    document.getElementsByTagName('head')[0].appendChild(link);
+    this.title.setTitle(head.title);
+    this.meta.addTag({ name: 'author', content: head.author });
+    this.meta.addTag({ name: 'keywords', content: head.keywords });
+    this.meta.addTag({ name: 'description', content: head.description });
+    const style = document.createElement('style');
+    style.innerHTML = head.style + head.page_code;
+    style.id = 'kb-style-'+this.req.pid;
+    document.head.appendChild(style);
+    const script = document.createElement('script');
+    script.src = '/assets/script/tracking.js';
+    script.async = true;
+    document.head.appendChild(script);
+  }
+
+  loadScript(code:any, targetElement:any) {
+    const container = document.createElement('div');
+    container.innerHTML = code;
+    const scriptElements = container.querySelectorAll('script');
+    scriptElements.forEach((scriptElement) => {
+      const script:any = document.createElement('script');
+      script.text = scriptElement.textContent;
+      targetElement.appendChild(script);
+    });
+    const styleElements = container.querySelectorAll('style');
+    styleElements.forEach((styleElement) => {
+      const style:any = document.createElement('style');
+      style.textContent = styleElement.textContent;
+      targetElement.appendChild(style);
+    });
+  }
 }
